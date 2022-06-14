db87c67df729e3a2de588e4fa5fbeefc47c7e93f
Randormeur <r.pirate94@gmail.com>
2022-06-14 08:48:35 +0200

    Changement middleware de redirection

--- middleware/adminaccess.js
+++ middleware/adminaccess.js
@@ -1,6 +1,5 @@
 export default function ({$auth, redirect}) {
    if ($auth.user.isAdmin!=1 && $auth.user.isAdmin!=2 ) {
-      process.client.alert("Accès non autorisé");
-      return redirect('/posts')
+      return redirect('/');
    } 
 }
