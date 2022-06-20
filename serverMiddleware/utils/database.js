2859be96101036cb9089b8bf20409f113104c6b2
Randormeur <r.pirate94@gmail.com>
2022-06-20 14:01:44 +0200

    correction

--- serverMiddleware/utils/database.js
+++ serverMiddleware/utils/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // Connection to MariaDB
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST || localhost,
+    host: process.env.DB_HOST || "localhost",
     dialect: process.env.DB_DIALECT || "mariadb",
     port: process.env.DB_PORT,
     dialectOptions: {
